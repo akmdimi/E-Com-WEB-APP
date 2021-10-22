63b731fe65e7c8e7a9cbb4693bbdd6f8f129907e
Imran <akmdimi@gmail.com>
2021-10-22 23:50:50 +0530

    T-shirt.

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,27 @@
 var express = require("express");
 var router = express.Router();
-const { signout, signup } = require("../controllers/auth");
+const { check } = require('express-validator');
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
-router.post("/signup", signup);
+//Signup validation
+router.post("/signup",[
+    check("name", "name should be atleast 3 char").isLength({min:3}),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be atleast 3 char").isLength({min:3}),
+
+], 
+signup);
 router.get("/signout", signout);
+
+router.post("/signin",[
+    check("email", "email is required").isEmail(),
+    check("password", "password field is required").isLength({min:3}),
+
+], 
+signin);
+router.get("/signout", signout);
+router.get("/testroute", isSignedIn, (req,res)=>{
+    res.send("A protected route")
+});
 
 module.exports = router;
